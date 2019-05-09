7cc17a3e534a7633a73db82c8b06e697ef4feef1
mouse\keiko <service@kame-kobo.com>
2019-05-10 00:40:53 +0900

    gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,45 @@
-var gulp = require("gulp");
+const gulp = require('gulp');
 
-var ejs = require("gulp-ejs");
-var efs = require("fs");
-var edata = require("gulp-data");
-var esass = require("gulp-sass");
-var epleeease = require("gulp-pleeease");
-var eplumber = require('gulp-plumber');
-var eimagemin = require("gulp-imagemin");
-var epngquant = require('imagemin-pngquant');
-var emozjpeg = require('imagemin-mozjpeg');
-var browserSync = require("browser-sync");
+const plumber = require('gulp-plumber');
+
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+const browserSync = require('browser-sync');
+const watch = require('gulp-watch');
+
+const ejs = require('gulp-ejs');
+const fs = require('fs');
+const data = require('gulp-data');
+const rename = require('gulp-rename');
+
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+const mozjpeg = require('imagemin-mozjpeg');
 
 let $ = require('gulp-load-plugins')();
 
-var DEV = "src",
-    PUBLIC = "dist";
-
-//browser sync
-gulp.task('browserSync', () => {
-    return browserSync.init({
-        server: {
-            baseDir: PUBLIC
-        },
-        port: 5000,
-        reloadOnRestart: true
-    });
-});
-// gulp.task("server", function() {
-//     browser({
-//         server: {
-//             baseDir: PUBLIC
-//         },
-//         port: 5000
-//     });
-// });
-
-// reload
-gulp.task("reload", (done) => {
-    browserSync.reload();
-    done();
-});
-
-//ejs
-gulp.task("ejs", function() {
-    var json = JSON.parse(fs.readFileSync('src/views/common/meta.json'));
-    return gulp.src(
-        [DEV + "/views/**/*.ejs",'!' + DEV + "/views/**/_*.ejs"]
-    )
-        .pipe($.plumber())
-        .pipe($.data(file => {
-            return {
-              'filename': file.path
-          }
-          }))
-        .pipe($.ejs({json},{},{ext:'.html'}))
-        .pipe(gulp.dest(PUBLIC))
-        // .pipe(browser.reload({stream:true}));
-});
+const DEV = 'src',
+    PUBLIC = 'dist';
 
 //style
-gulp.task("style", function() {
-  return (
-    gulp.src(DEV + "/assets/styles/**/*.scss")
-        .pipe(plumber())
-        .pipe(sass({outputStyle: "compressed"}))
-        .pipe(pleeease({
-            fallbacks: {
-                autoprefixer: ["last 2 version", "ie 11"]
-            },
-            minifier: false
-        }))
-      .pipe(gulp.dest(PUBLIC + "/assets/styles"))
-  );
+gulp.task('style', () => {
+    return gulp.src(DEV + '/assets/styles/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass({outputStyle: 'compressed'})).on('error', sass.logError)
+    .pipe(autoprefixer({browsers: ['last 2 versions', 'ie >= 11']}))
+    .pipe(gulp.dest(PUBLIC + '/assets/styles'))
 });
 
-//copy
-gulp.task("js", function() {
-    return gulp.src(DEV + "/assets/scripts/**/*.js")
-        .pipe(gulp.dest(PUBLIC + "/assets/scripts"));
+//js
+gulp.task('js', function() {
+    return gulp.src(DEV + '/assets/scripts/**/*.js')
+        .pipe(gulp.dest(PUBLIC + '/assets/scripts'));
 });
 
 //images
-gulp.task("images", function() {
-    return gulp.src(DEV + "/assets/images/**/*.{png,jpg,gif,svg}")
+gulp.task('images', function() {
+    return gulp.src(DEV + '/assets/images/**/*.{png,jpg,gif,svg}')
     .pipe(imagemin([
         pngquant({
             quality: [ 0.65, 0.8 ],
@@ -96,16 +53,44 @@ gulp.task("images", function() {
         imagemin.svgo(),
         imagemin.optipng(),
         imagemin.gifsicle()
-                ])) // 画像の圧縮処理
-    .pipe(gulp.dest((PUBLIC + "/assets/images")));
+    ]))
+    .pipe(gulp.dest((PUBLIC + '/assets/images')));
 });
 
-//watch
-gulp.task("default", gulp.series(
-    gulp.parallel("ejs","style","js","images"),
-    "server", function() {
-    gulp.watch(DEV + "/views/**/*.ejs",gulp.task("ejs"));
-    gulp.watch(DEV + "/assets/styles/**/*.scss",gulp.task("style"));
-    gulp.watch(DEV + "/assets/scripts/**/*.js",gulp.task("js"));
-    gulp.watch(DEV + "/assets/images/**/*",gulp.task("images"));
-}));
+//ejs
+gulp.task('ejs', () => {
+    var json = JSON.parse(fs.readFileSync(DEV +'/views/common/meta.json'));
+    return gulp.src(
+        [DEV + '/views/**/*.ejs','!' + DEV + '/views/**/_*.ejs']
+    )
+        .pipe($.plumber())
+        .pipe($.data(file => {
+            return {
+              'filename': file.path
+          }
+          }))
+        .pipe($.ejs())
+        .pipe(rename({extname: '.html'}))
+        .pipe(gulp.dest(PUBLIC))
+});
+
+// sever
+gulp.task('browser-sync', () => {
+    browserSync({
+        server: {
+            baseDir: PUBLIC
+        },
+        port: 5000,
+        // startPath: `${BASE_PATH}`,
+        // ghostMode: false
+    });
+    watch([DEV + '/views/**/*.ejs'], gulp.series('ejs', browserSync.reload));
+    watch([DEV + '/assets/styles/**/*.scss'], gulp.series('style', browserSync.reload));
+    watch([DEV + '/assets/scripts/**/*.js'], gulp.series('js', browserSync.reload));
+    watch([DEV + '/assets/images/**/*.js'], gulp.series('images', browserSync.reload));
+});
+gulp.task('server', gulp.series('browser-sync'));
+
+// default
+gulp.task('build', gulp.parallel('ejs','style','js','images'));
+gulp.task('default', gulp.series('build', 'server'));
